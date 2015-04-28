e88a9b12ff389c3f5762c383668b20599af48ba5
Pierre-Charles David <pierre-charles.david@obeo.fr>
2015-04-28 11:14:56 +0200

    [DRAFT cleanup] Move hasControlledRootIn into ResourceQueryInternal
    Change-Id: I2cea358dde907d401141bc68a5c47bf40cd393c6
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/query/ResourceQueryInternal.java
+++ plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/query/ResourceQueryInternal.java
@@ -13,6 +13,8 @@
 
 package org.eclipse.sirius.business.internal.query;
 
+import java.util.Collection;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
@@ -44,6 +46,26 @@ public class ResourceQueryInternal {
      */
     public ResourceQueryInternal(Resource resource) {
         this.resource = resource;
+    }
+
+    /**
+     * Checks if the current resource has any of its root controlled by one of
+     * the specified resources.
+     * 
+     * @param scope
+     *            the potential controlling resources.
+     * @return <code>true</code> if at least one of this resource's root
+     *         elements is actually controlled by one of the specified resource.
+     */
+    public boolean hasControlledRootIn(Collection<Resource> scope) {
+        for (EObject root : resource.getContents()) {
+            EObject container = root.eContainer();
+            Resource containerResource = container != null ? container.eResource() : null;
+            if (containerResource != resource && scope.contains(containerResource)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
